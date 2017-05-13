65bad1211f18398ad740b604622b94971a4c6dbe
Emad J. Maroun <emoun.open@gmail.com>
2017-05-13 23:23:04 +0200

    Documented PowerSet

--- src/main/java/dk/emoun/progysis/lattices/EnumPowerSet.java
+++ src/main/java/dk/emoun/progysis/lattices/EnumPowerSet.java
@@ -6,14 +6,27 @@ public abstract class EnumPowerSet<S extends EnumPowerSet<S,E>, E extends Enum<E
 
 	
 //Constructors
+	/**
+	 * Creates the bottom element of the lattices.
+	 */
 	public EnumPowerSet(){
 		super();
 	}
 	
+	/**
+	 * Creates an element with the given values as its set.
+	 * Duplications are ignored.
+	 * @param enums
+	 * Values of the set.
+	 */
 	public EnumPowerSet(E...enums){
 		super(enums);
 	}
 	
+	/**
+	 * Creates an element in the lattice with the given set.
+	 * @param enumSet
+	 */
 	public EnumPowerSet(Set<E> enumSet){
 		super(enumSet);
 	}
--- src/main/java/dk/emoun/progysis/lattices/PowerSet.java
+++ src/main/java/dk/emoun/progysis/lattices/PowerSet.java
@@ -4,15 +4,42 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Implements general powerset lattices.
+ * A Powerset is a lattice where the empty set is bottom, and top is a set of values.
+ * The lattice is orderes such that all non-top elements of the lattice contain
+ * a subset of the top set.<br>
+ * Given two elements in the powerset e1 and e2, e1.compare(e2) == true if 
+ * e1 is a subset of e2.
+ *  
+ * @param <S>
+ * The type of the powerset.
+ * @param <V>
+ * The type of values in the sets of the powerset elements.
+ */
 public abstract class PowerSet<S extends PowerSet<S,V>, V> extends LatticeElement<S>{
 	
 //fields
+	
+	/**
+	 * The set of values comprising the lattice element
+	 */
 	private Set<V> valueSet;
 //Constructors
+	
+	/**
+	 * Creates the bottom element of the lattices.
+	 */
 	public PowerSet(){
 		this.valueSet = Collections.emptySet();
 	}
 	
+	/**
+	 * Creates an element with the given values as its set.
+	 * Duplications are ignored.
+	 * @param values
+	 * Values of the set.
+	 */
 	public PowerSet(V...values){
 		Set<V> temp = new HashSet<V>();
 		for(V s: values){
@@ -21,22 +48,38 @@ public abstract class PowerSet<S extends PowerSet<S,V>, V> extends LatticeElemen
 		setValueSetImmutable(temp);
 	}
 	
-	public PowerSet(Set<V> enumSet){
-		setValueSetImmutable(enumSet);
+	/**
+	 * Creates an element in the lattice with the given set.
+	 * @param valueSet
+	 */
+	public PowerSet(Set<V> valueSet){
+		setValueSetImmutable(valueSet);
 	}
 	
 //abstract methods
-
+	
+	/**
+	 * Creates a new element in the lattice with the given set.
+	 * @param valueSet
+	 * The set of the element
+	 * @return
+	 * The created element
+	 */
 	public abstract S createPowerSet(Set<V> valueSet);
 	
 //methods
-
+	
+	/** 
+	 * @return
+	 * all the values of the set in the element
+	 */
 	public Set<V> getValueSet(){
 		return valueSet;
 	}
 		
 	
 //Overriding methods
+	@Override
 	public S getBottom(){
 		return createPowerSet(Collections.emptySet());
 	}
@@ -81,6 +124,10 @@ public abstract class PowerSet<S extends PowerSet<S,V>, V> extends LatticeElemen
 	}
 	
 //Private Methods
+	/**
+	 * Sets the valueSet to an unmoddifiable version of the given set.
+	 * @param enumSet
+	 */
 	private void setValueSetImmutable(Set<V> enumSet) {
 		this.valueSet = Collections.unmodifiableSet(enumSet);
 	}
